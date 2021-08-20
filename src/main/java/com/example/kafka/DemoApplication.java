17bb24056d2ba0b35e5753564ddf7558238ec949
mderzhavin <mulkapestri@gmail.com>
2021-08-20 10:39:01 +0300

    Initial commit

---
+++ src/main/java/com/example/kafka/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@EnableKafka
+public class DemoApplication {
+
+	@KafkaListener(topics="msg")
+	public void msgListener(String msg){
+		System.out.println(msg);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
